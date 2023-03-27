c7108ba8761b2364906cfc83bf462fd4255f5566
JairAssisDev <jairv141@gmail.com>
2023-03-27 13:13:11 -0300

    IMERSÃO JAVA

---
+++ src/main/java/test/AppTest.java
@@ -0,0 +1,32 @@
+package test;
+
+//import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class AppTest {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String url = "https://gist.githubusercontent.com/lucasfugisawa/cbb0d10ee3901bd0541468e431c629b3/raw/1fe1f3340dfe5b5876a209c0e8226d090f6aef10/Top250Movies.json";
+		URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request= HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request,BodyHandlers.ofString());
+        String body = response.body();
+
+        //extrair só os dados que interessam (titulo , poster,classificaçao)
+        var parser  = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        //System.out.println(listaDeFilmes.get(0));
+        for (Map<String,String>filme :listaDeFilmes){
+            System.out.println(filme.get("title"));
+        }
+	}
+
+}
